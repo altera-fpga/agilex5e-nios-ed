37a4b15f90f8973b43a8754f642e7ddfcae1e2ef
Padmanabhan Ayyaswamy <sai.padmanabhan@gmail.com>
2025-02-10 12:25:58 +0530

    Updating comments

--- niosv_g/niosv_g_helloworld/sources/sw/app/main.c
+++ niosv_g/niosv_g_helloworld/sources/sw/app/main.c
@@ -3,10 +3,10 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-#include <stdio.h> // Standard I/O functions
-#include <unistd.h> // Standard library for usleep function
+#include <stdio.h>
+#include <unistd.h>
 
-// Function to print "Hello world" messages in a loop
+// Function to print a message multiple times in a loop
 void looper() {
     for (int i = 0; i < 10; ++i) {
         printf("Hello world, this is the Nios V/g cpu checking in %d...\n", i);
@@ -15,8 +15,14 @@ void looper() {
 
 // Main function
 int main() {
-    looper(); // Call the looper function
-    usleep(1000); // Sleep for 1000 microseconds
-    printf("Bye world!\n"); // Print goodbye message
-    return 0; // Exit the application
+    // Call the looper function to print messages
+    looper();
+    
+    // Introduce a short delay before program exits
+    usleep(1000);
+    
+    // Print exit message
+    printf("Bye world!\n");
+    
+    return 0;
 }

9c3a382589cd9ef48ea29b9c6f380b70eb1f5449
Padmanabhan Ayyaswamy <sai.padmanabhan@gmail.com>
2025-02-10 12:29:14 +0530

    Updating comments

--- niosv_c/niosv_c_helloworld_ocm_mem_test/sources/sw/app/main.c
+++ niosv_c/niosv_c_helloworld_ocm_mem_test/sources/sw/app/main.c
@@ -7,47 +7,6 @@
 
 #define OCM_BASE INTEL_ONCHIP_MEMORY_0_BASE
 
-#if 0
-// Using usleep as the delay function
-int usleep(useconds_t usec);
-
-// Function to test PIO functionality
-int pio_test()
-{
-    int count = 0;
-    int i;
-    int pio_err = 0;
-    printf("Application to toggle the PIOs- [4:0] \n");
-
-    while (count < 64)
-    {
-        i = count & 0xf;
-        
-        // Write data to the PIO register at base address PIO_0_BASE
-        IOWR_ALTERA_AVALON_PIO_DATA(PIO_0_BASE, count & 0xf);
-        
-        // Introduce a short delay
-        usleep(100);
-        
-        // Read back data from the PIO register
-        printf("DATA READBACK FROM PIO_0_BASE is 0x%x \n", IORD_ALTERA_AVALON_PIO_DATA(PIO_0_BASE));
-
-        // Verify if the written data matches the read data
-        if (i != IORD_ALTERA_AVALON_PIO_DATA(PIO_0_BASE))
-        {
-            printf("Data MISMATCH - TEST FAILED \n");
-            pio_err = pio_err + 1;
-        }
-        else
-        {
-            printf("DATA MATCHED - TEST PASSED \n");
-        }
-        count++;
-    }
-    return pio_err;
-}
-#endif
-
 int main()
 {
     int p;
@@ -87,23 +46,6 @@ int main()
     }
 
     printf("Memory Test Complete \n");
-
-#if 0
-    int pio_fail_flag;
-    
-    // Run the PIO test function
-    pio_fail_flag = pio_test();
-    
-    // Check the result of the PIO test
-    if (pio_fail_flag != 0)
-    {
-        printf("NIOSV-PIO Test failed with PIO_ERR = %d\n", pio_fail_flag);
-    }
-    else
-    {
-        printf("NIOSV-PIO Test PASSED \n");
-    }
-#endif
 
     // Read and print the System ID from the peripheral core
     printf("Print the value of System ID \n");
